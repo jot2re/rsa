5b73c4d323fdc708a57d959d4050a6e66b1b8650
Tore Frederiksen <jot2re@gmail.com>
2023-03-17 17:17:20 +0100

    incremental commit of constant round membership. Does NOT work

--- src/main/java/dk/jot2re/rsa/our/sub/membership/MembershipConst.java
+++ src/main/java/dk/jot2re/rsa/our/sub/membership/MembershipConst.java
@@ -2,9 +2,11 @@ package dk.jot2re.rsa.our.sub.membership;
 
 import dk.jot2re.network.NetworkException;
 import dk.jot2re.rsa.bf.BFParameters;
+import dk.jot2re.rsa.our.RSAUtil;
+import dk.jot2re.rsa.our.sub.invert.Invert;
 
 import java.math.BigInteger;
-import java.util.List;
+import java.util.*;
 
 public class MembershipConst implements IMembership {
     private final BFParameters params;
@@ -14,6 +16,95 @@ public class MembershipConst implements IMembership {
     }
 
     public BigInteger execute(BigInteger xShare, List<BigInteger> set, BigInteger modulo) throws NetworkException {
-        return null;
+        // TODO handle edgecase when set is smaller than 2
+        int m = set.size();
+        // Step 1; sample
+        BigInteger rho = RSAUtil.sample(params, modulo);
+        Map<Integer, BigInteger> rShares = new HashMap<>(m);
+        rShares.put(1, RSAUtil.sample(params, modulo));
+        Map<Integer, BigInteger> alphaShares = new HashMap<>(m-1);
+        for (int i = 2; i <= m; i++) {
+            rShares.put(i, RSAUtil.sample(params, modulo));
+            alphaShares.put(i, RSAUtil.sample(params, modulo));
+        }
+        // Step 2; invert
+        Invert inverter = new Invert(params);
+        Map<Integer, BigInteger> invertedRShares = new HashMap<>(m);
+        for (int i: rShares.keySet()) {
+            invertedRShares.put(i, inverter.execute(rShares.get(i), modulo));
+        }
+        Map<Integer, BigInteger> invertedAlphaShares = new HashMap<>(m-1);
+        for (int i: alphaShares.keySet()) {
+            invertedAlphaShares.put(i, inverter.execute(alphaShares.get(i), modulo));
+        }
+        // Step 3; compute products
+        Map<Integer, BigInteger> tShares = new HashMap<>(m);
+        for (int i: invertedRShares.keySet()) {
+            tShares.put(i, params.getMult().mult(xShare, invertedRShares.get(i), modulo));
+        }
+        Map<Integer, BigInteger> vShares = new HashMap<>(m-1);
+        for (int i: alphaShares.keySet()) {
+            BigInteger temp = params.getMult().mult(alphaShares.get(i), tShares.get(i), modulo);
+            vShares.put(i, params.getMult().mult(temp, rShares.get(1), modulo));
+        }
+        Map<Integer, BigInteger> wShares = new HashMap<>(m-1);
+        for (int i = 2; i <= m; i++) {
+            wShares.put(i, params.getMult().mult(tShares.get(i-1), rShares.get(i), modulo));
+        }
+        // ... and open these
+        Map<Integer, BigInteger> vValues = new HashMap<>(m-1);
+        for (int i: vShares.keySet()) {
+            vValues.put(i, RSAUtil.open(params, vShares.get(i), modulo));
+        }
+        Map<Integer, BigInteger> wValues = new HashMap<>(m-1);
+        for (int i: wShares.keySet()) {
+            wValues.put(i, RSAUtil.open(params, wShares.get(i), modulo));
+        }
+        // Step 4; compute y values
+        Map<Integer, BigInteger> yValues = new HashMap<>(m-1);
+        for (int i: vValues.keySet()) {
+            BigInteger currentY = vValues.get(i);
+            for (int j = 2; j <= i; j++) {
+                currentY.multiply(wValues.get(j)).mod(modulo);
+            }
+            yValues.put(i, currentY);
+        }
+        // Step 5; compute coefficients
+        BigInteger[] coef = computePolyConsts(set, modulo);
+        // Step 6; compute result
+        BigInteger zShare = coef[1].multiply(xShare).mod(modulo);
+        if (params.getMyId() == 0) {
+            zShare = zShare.add(coef[0]).mod(modulo);
+        }
+        for (int i = 2; i < coef.length; i++) {
+            zShare = zShare.add(yValues.get(i).multiply(coef[i]).multiply(alphaShares.get(i))).mod(modulo);
+        }
+        zShare = zShare.add(alphaShares.get(m).multiply(yValues.get(m))).mod(modulo);
+        return zShare; //params.getMult().mult(zShare, rho, modulo);
+    }
+
+    protected BigInteger[] computePolyConsts(List<BigInteger> roots, BigInteger modulo) {
+        // Each coefficient is defined to be the previous coefficient of same degree, multiplied with current root,
+        // adding with the coefficient in the previous iteration that is one degree smaller.
+        // coef[i] = root[i]*prevCoef[i]+prevCoef[i-1]
+        BigInteger[] lastCoef = new BigInteger[roots.size()];
+        lastCoef[0] = roots.get(0).negate().mod(modulo);
+        for (int i = 1; i < roots.size(); i++) {
+            BigInteger curRoot = roots.get(i).negate();
+            BigInteger[] curCoef = new BigInteger[roots.size()];
+            for (int j = 0; j <= i; j++) {
+                BigInteger sum = BigInteger.ZERO;
+                if (j >= 1) {
+                    sum = lastCoef[j-1];
+                }
+                BigInteger prod = BigInteger.ONE;
+                if (j < i) {
+                    prod = lastCoef[j];
+                }
+                curCoef[j] = curRoot.multiply(prod).add(sum).mod(modulo);
+            }
+            lastCoef = curCoef;
+        }
+        return lastCoef;
     }
 }
--- src/test/java/dk/jot2re/rsa/our/sub/membership/MembershipProtocolTest.java
+++ src/test/java/dk/jot2re/rsa/our/sub/membership/MembershipProtocolTest.java
@@ -2,6 +2,7 @@ package dk.jot2re.rsa.our.sub.membership;
 
 import dk.jot2re.AbstractProtocolTest;
 import dk.jot2re.rsa.bf.BFParameters;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,17 +13,17 @@ import java.util.Map;
 import java.util.concurrent.Future;
 
 import static dk.jot2re.rsa.RSATestUtils.share;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MembershipProtocolTest extends AbstractProtocolTest {
 
     @ParameterizedTest
-    @CsvSource({"2,linear", "3,linear", "5,linear", "2,log", "3,log", "5,log"})
+    @CsvSource({"2,const"})// "3,linear", "5,linear", "2,log", "3,log", "5,log"})
     public void sunshine(int parties, String type) throws Exception {
-        BigInteger input = BigInteger.valueOf(42);
-        List<BigInteger> set = Arrays.asList(BigInteger.valueOf(12), BigInteger.valueOf(2544), BigInteger.valueOf(42), BigInteger.valueOf(1000));
-        Map<Integer, BigInteger> shares = share(input, parties, modulo, rand);
+        BigInteger mod = BigInteger.valueOf(137);
+        BigInteger input = BigInteger.valueOf(2);
+        List<BigInteger> set = Arrays.asList(BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3));
+        Map<Integer, BigInteger> shares = share(input, parties, mod, rand);
 
         AbstractProtocolTest.RunProtocol<BigInteger> protocolRunner = (param) -> {
             IMembership protocol;
@@ -30,16 +31,18 @@ public class MembershipProtocolTest extends AbstractProtocolTest {
                 protocol = new MembershipLinear((BFParameters) param);
             } else if (type.equals("log")) {
                 protocol = new MembershipLog((BFParameters) param);
+            } else if (type.equals("const")) {
+                protocol = new MembershipConst((BFParameters) param);
             } else {
                 throw new RuntimeException("unknown type");
             }
-            return protocol.execute(shares.get(param.getMyId()), set, modulo);
+            return protocol.execute(shares.get(param.getMyId()), set, mod);
         };
 
         AbstractProtocolTest.ResultCheck<BigInteger> checker = (res) -> {
             BigInteger finalValue = BigInteger.ZERO;
             for (Future<BigInteger> cur : res) {
-                finalValue = finalValue.add(cur.get()).mod(modulo);
+                finalValue = finalValue.add(cur.get()).mod(mod);
                 assertNotEquals(BigInteger.ONE, cur.get());
             }
             // Ensure the result is 0
@@ -62,6 +65,8 @@ public class MembershipProtocolTest extends AbstractProtocolTest {
                 protocol = new MembershipLinear((BFParameters) param);
             } else if (type.equals("log")) {
                 protocol = new MembershipLog((BFParameters) param);
+            } else if (type.equals("const")) {
+                protocol = new MembershipConst((BFParameters) param);
             } else {
                 throw new RuntimeException("unknown type");
             }
@@ -79,6 +84,53 @@ public class MembershipProtocolTest extends AbstractProtocolTest {
         };
 
         runProtocolTest(DEFAULT_BIT_LENGTH, DEFAULT_STAT_SEC, parties, protocolRunner, checker);
+    }
+
+    // TODO test for equal roots
+    @Test
+    void polyTest1() {
+        MembershipConst membership = new MembershipConst(params.get(0));
+        BigInteger modulus = BigInteger.valueOf(4091);
+        List<BigInteger> roots = Arrays.asList(BigInteger.valueOf(1));
+        List<BigInteger> ref = Arrays.asList(BigInteger.valueOf(-1).mod(modulus));
+        BigInteger[] res = membership.computePolyConsts(roots, modulus);
+        assertArrayEquals(ref.toArray(), res);
+    }
+
+    @Test
+    void polyTest2() {
+        MembershipConst membership = new MembershipConst(params.get(0));
+        BigInteger modulus = BigInteger.valueOf(4091);
+        List<BigInteger> roots = Arrays.asList(BigInteger.valueOf(1), BigInteger.valueOf(2));
+        List<BigInteger> ref = Arrays.asList(BigInteger.valueOf(2), BigInteger.valueOf(-3).mod(modulus));
+        BigInteger[] res = membership.computePolyConsts(roots, modulus);
+        assertArrayEquals(ref.toArray(), res);
+    }
+
+    @Test
+    void polyTest3() {
+        MembershipConst membership = new MembershipConst(params.get(0));
+        BigInteger modulus = BigInteger.valueOf(4091);
+        List<BigInteger> roots = Arrays.asList(BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3));
+        List<BigInteger> ref = Arrays.asList(BigInteger.valueOf(-6).mod(modulus), BigInteger.valueOf(11).mod(modulus), BigInteger.valueOf(-6).mod(modulus));
+        BigInteger[] res = membership.computePolyConsts(roots, modulus);
+        assertArrayEquals(ref.toArray(), res);
+    }
+
+    @Test
+    void polyTest4() {
+        MembershipConst membership = new MembershipConst(params.get(0));
+        BigInteger modulus = BigInteger.valueOf(4091);
+        List<BigInteger> roots = Arrays.asList(BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(5), BigInteger.valueOf(6));
+        List<BigInteger> ref = Arrays.asList(
+                BigInteger.valueOf(720).mod(modulus),
+                BigInteger.valueOf(-1764).mod(modulus),
+                BigInteger.valueOf(1624).mod(modulus),
+                BigInteger.valueOf(-735).mod(modulus),
+                BigInteger.valueOf(175).mod(modulus),
+                BigInteger.valueOf(-21).mod(modulus));
+        BigInteger[] res = membership.computePolyConsts(roots, modulus);
+        assertArrayEquals(ref.toArray(), res);
     }
 
 }
