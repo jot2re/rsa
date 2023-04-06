8eb13ec4433a560a9e028fa1291e8fecafe888cc
Tore Frederiksen <jot2re@gmail.com>
2023-04-06 16:09:15 +0200

    feat: setup multiparty mult protocol

--- src/main/java/dk/jot2re/mult/ot/ips/IPSMult.java
+++ src/main/java/dk/jot2re/mult/ot/ips/IPSMult.java
@@ -1,23 +1,25 @@
 package dk.jot2re.mult.ot.ips;
 
 import dk.jot2re.mult.IMult;
-import dk.jot2re.mult.ot.ot.otextension.OtExtensionResourcePool;
+import dk.jot2re.mult.ot.MultResourcePool;
 import dk.jot2re.mult.ot.ot.otextension.RotFactory;
 import dk.jot2re.network.INetwork;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static dk.jot2re.mult.ot.util.Fiddling.ceil;
 
 
 public class IPSMult implements IMult {
     private static final int DEFAULT_BATCH_SIZE = 1048576;
-    private final OtExtensionResourcePool resources;
+    private final MultResourcePool resources;
     private final boolean safeExpansion;
     private final int adjustedBatchSize;
     private INetwork network;
-    private IPSOTFactory factory;
+    private Map<Integer, IPSOTFactory> factory;
     private int amountBits;
     private int expansionSizeBytes;
 
@@ -25,29 +27,36 @@ public class IPSMult implements IMult {
      * If safeExpansion is false, then it is required that the modulo is exponentially close to a two-power.
      * batchSize must be a 2-power
      */
-    public IPSMult(OtExtensionResourcePool resources, int batchSize, boolean safeExpansion) {
+    public IPSMult(MultResourcePool resources, int batchSize, boolean safeExpansion) {
         this.resources = resources;
         this.safeExpansion = safeExpansion;
-        this.adjustedBatchSize = batchSize - resources.getComputationalSecurityParameter()- resources.getLambdaSecurityParam();
+        this.adjustedBatchSize = batchSize - resources.getCompSec()- resources.getStatSec();
     }
 
-    public IPSMult(OtExtensionResourcePool resources) {
+    public IPSMult(MultResourcePool resources) {
         this.resources = resources;
         this.safeExpansion = true;
-        this.adjustedBatchSize = DEFAULT_BATCH_SIZE - resources.getComputationalSecurityParameter()- resources.getLambdaSecurityParam();
+        this.adjustedBatchSize = DEFAULT_BATCH_SIZE - resources.getCompSec()- resources.getStatSec();
     }
 
     @Override
     public void init(INetwork network) {
+        //todo fix consistency in when network is supplied
         if (this.factory == null) {
-            RotFactory rotFactory = new RotFactory(resources, network);
-            this.factory = new IPSOTFactory(rotFactory, resources, network, adjustedBatchSize);
-            if (resources.getMyId() == 0) {
-                this.factory.initSender();
-                this.factory.initReceiver();
-            } else {
-                this.factory.initReceiver();
-                this.factory.initSender();
+            this.factory = new HashMap<>(resources.getParties()-1);
+            for (int i = 0; i < resources.getParties(); i++) {
+                if (resources.getMyId() != i) {
+                    RotFactory rotFactory = new RotFactory(resources.getOtResources(i), network);
+                    IPSOTFactory ipsFactory = new IPSOTFactory(rotFactory, resources.getOtResources(i), network, adjustedBatchSize);
+                    if (resources.getMyId() < i) {
+                        ipsFactory.initSender();
+                        ipsFactory.initReceiver();
+                    } else {
+                        ipsFactory.initReceiver();
+                        ipsFactory.initSender();
+                    }
+                    factory.put(i, ipsFactory);
+                }
             }
         }
         this.network = network;
@@ -55,26 +64,31 @@ public class IPSMult implements IMult {
 
     @Override
     public BigInteger mult(BigInteger shareA, BigInteger shareB, BigInteger modulo) {
-        this.amountBits = resources.getComputationalSecurityParameter()+resources.getLambdaSecurityParam();
+        this.amountBits = resources.getCompSec()+resources.getStatSec();
         if (safeExpansion) {
-            this.expansionSizeBytes = (modulo.bitLength()/8) + ceil(resources.getLambdaSecurityParam(), 8);
+            this.expansionSizeBytes = (modulo.bitLength()/8) + ceil(resources.getStatSec(), 8);
         } else {
             this.expansionSizeBytes = modulo.bitLength()/8;
         }
-        BigInteger senderShare, receiverShare;
-        if (resources.getMyId() == 0) {
-            senderShare = senderRole(shareA, modulo);
-            receiverShare = receiverRole(shareB, modulo);
-        } else {
-            receiverShare = receiverRole(shareB, modulo);
-            senderShare = senderRole(shareA, modulo);
+        BigInteger partialRes = shareA.multiply(shareB).mod(modulo);
+        for (int i = 0; i < resources.getParties(); i++) {
+            if (resources.getMyId() != i) {
+                BigInteger senderShare, receiverShare;
+                if (resources.getMyId() < i) {
+                    senderShare = senderRole(factory.get(i).getSender(), shareA, modulo);
+                    receiverShare = receiverRole(factory.get(i).getReceiver(), shareB, modulo);
+                } else {
+                    receiverShare = receiverRole(factory.get(i).getReceiver(), shareB, modulo);
+                    senderShare = senderRole(factory.get(i).getSender(), shareA, modulo);
+                }
+                partialRes = senderShare.add(receiverShare).add(partialRes);
+            }
         }
-        BigInteger res = receiverShare.add(senderShare).add(shareA.multiply(shareB)).mod(modulo);
-        return res;
+        return partialRes.mod(modulo);
     }
 
-    private BigInteger senderRole(BigInteger value, BigInteger modulo) {
-        List<BigInteger> shares = factory.getSender().send(value, modulo, expansionSizeBytes);
+    private BigInteger senderRole(IPSOTSender sender, BigInteger value, BigInteger modulo) {
+        List<BigInteger> shares = sender.send(value, modulo, expansionSizeBytes);
         BigInteger z = BigInteger.ZERO;
         for (int i = 0; i < amountBits; i++) {
             z = z.add(shares.get(i));
@@ -83,8 +97,8 @@ public class IPSMult implements IMult {
         return z;
     }
 
-    private BigInteger receiverRole(BigInteger value, BigInteger modulo) {;
-        List<BigInteger> receivedShares = factory.getReceiver().receive(value, modulo, expansionSizeBytes);
+    private BigInteger receiverRole(IPSOTReceiver receiver, BigInteger value, BigInteger modulo) {;
+        List<BigInteger> receivedShares = receiver.receive(value, modulo, expansionSizeBytes);
         BigInteger z = BigInteger.ZERO;
         for (int i = 0; i < amountBits; i++) {
             z = z.add(receivedShares.get(i));
--- src/test/java/dk/jot2re/mult/ot/ips/IPSMultTest.java
+++ src/test/java/dk/jot2re/mult/ot/ips/IPSMultTest.java
@@ -1,11 +1,15 @@
 package dk.jot2re.mult.ot.ips;
 
 import dk.jot2re.mult.IMult;
-import dk.jot2re.mult.ot.ot.otextension.OtExtensionResourcePool;
-import dk.jot2re.mult.ot.ot.otextension.OtExtensionTestContext;
+import dk.jot2re.mult.ot.MultResourcePool;
+import dk.jot2re.mult.ot.ot.base.DummyOt;
+import dk.jot2re.mult.ot.ot.base.Ot;
+import dk.jot2re.mult.ot.util.AesCtrDrbg;
+import dk.jot2re.mult.ot.util.Drbg;
 import dk.jot2re.network.DummyNetworkFactory;
 import dk.jot2re.network.INetwork;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigInteger;
 import java.util.*;
@@ -15,43 +19,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IPSMultTest {
+    // todo refactor and consolidate with Gilboa
     private static final int COMP_SEC = 128;
     private static final int STAT_SEC = 40;
-    private static final int DEFAULT_BIT_LENGTH = 8192;// MUST be two-power
+    private static final int DEFAULT_BIT_LENGTH = 2048;// MUST be two-power
 
     public static Map<Integer, IMult> getMults(int parties, int comp_sec, int statSec, boolean safeExpansion) throws ExecutionException, InterruptedException {
         // todo generalize to more than 2
         DummyNetworkFactory netFactory = new DummyNetworkFactory(parties);
         Map<Integer, INetwork> networks = netFactory.getNetworks();
+        Map<Integer, Future<IMult>> mults = new HashMap<>(parties);
 
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
-        Future<IMult> multZero = executor.submit(() -> {
-            OtExtensionResourcePool pool = (new OtExtensionTestContext(0,1, comp_sec, statSec, networks.get(0))).createResources(0);
-            IMult mult = new IPSMult(pool, 32*DEFAULT_BIT_LENGTH, safeExpansion);
-            mult.init(networks.get(0));
-            return mult;
-        });
-        Future<IMult> multOne = executor.submit(() -> {
-            OtExtensionResourcePool pool =(new OtExtensionTestContext(1,0, comp_sec, statSec, networks.get(1))).createResources(0);
-            IMult mult = new IPSMult(pool, 32*DEFAULT_BIT_LENGTH, safeExpansion);
-            mult.init(networks.get(1));
-            return mult;
-        });
+        for (int i = 0; i < parties; i++) {
+            int finalI = i;
+            Future<IMult> curMult = executor.submit(() -> {
+                Map<Integer, Ot> ots = new HashMap<>(parties-1);
+                for (int j = 0; j < parties; j++) {
+                    if (finalI != j) {
+                        ots.put(j, new DummyOt(j, networks.get(finalI)));
+                    }
+                }
+                byte[] seed = new byte[32];
+                seed[0] = (byte) finalI;
+                Drbg rand = new AesCtrDrbg(seed);
+                MultResourcePool pool = new MultResourcePool(ots, finalI, comp_sec, statSec, networks.get(finalI), rand);
+                // the constant mult be a 2-power larger than the amount of parties
+                IMult mult = new IPSMult(pool, 32 * DEFAULT_BIT_LENGTH, safeExpansion);
+                mult.init(networks.get(finalI));
+                return mult;
+            });
+            mults.put(i, curMult);
+        }
         executor.shutdown();
         assertTrue(executor.awaitTermination(30000, TimeUnit.SECONDS));
-        Map<Integer, IMult> mults = new HashMap<>(parties);
-        mults.put(0, multZero.get());
-        mults.put(1, multOne.get());
-        return mults;
+        Map<Integer, IMult> res = new HashMap<>(parties);
+        for (int i: mults.keySet()) {
+            res.put(i, mults.get(i).get());
+        }
+        return res;
     }
 
-    @Test
-    void sunshine() throws Exception {
-        int parties = 2;
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3, 5})
+    void sunshine(int parties) throws Exception {
         BigInteger modulo = BigInteger.TWO.pow(DEFAULT_BIT_LENGTH).subtract(BigInteger.ONE);
         BigInteger[] A = new BigInteger[parties];
         BigInteger[] B = new BigInteger[parties];
-        Map<Integer, IMult> mults = getMults(2, COMP_SEC, STAT_SEC, false);
+        Map<Integer, IMult> mults = getMults(parties, COMP_SEC, STAT_SEC, false);
         Random rand = new Random(42);
         for (int i = 0; i < parties; i++) {
             A[i] = new BigInteger(DEFAULT_BIT_LENGTH, rand);
